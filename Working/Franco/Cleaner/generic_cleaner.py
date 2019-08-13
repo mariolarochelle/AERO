1fe32bd4e7c3fd8f6ca96ea88bec155b505f8bf7
Franco Piergallini Guida <francopierguida@gmail.com>
2019-08-13 16:09:03 -0300

    Graficos

--- Working/Franco/Cleaner/generic_cleaner.py
+++ Working/Franco/Cleaner/generic_cleaner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import pandas as pd
@@ -10,15 +10,17 @@ import sys
 import os
 
 
-# In[28]:
+# In[107]:
 
 
 #raw_data = pd.read_csv("../../../Data/GRUPO DRAGON 2018/ech2/data/000960_Vaquerias_Jalisco_meas_2017.09.04-2017.10.03.txt", encoding = "utf8")
 #raw_data = pd.read_csv("../../../Data/GRUPO DRAGON 2018/ech1 18092018/ech1 18092018.txt", encoding = "utf8")
 #raw_data = pd.read_csv("../../../Data/GRUPO DRAGON 2018/ech2/data/001082__meas_2017.10.03-2018.01.23.txt", encoding = "utf8")
 
+#raw_data = pd.read_csv("C:/Users/Franco/Documents/AnacondaProjects/Github/AERO/Data/Datos a limpiar/000960_Vaquerias,_Jalisco_meas_2017.09.04-2017.10.03.txt", encoding = "utf8")
 
-# In[22]:
+
+# In[108]:
 
 
 searchfor = ['timestamp', 'otherwordtomatch']
@@ -28,7 +30,7 @@ def search_timestamp(dataframe):
     index_matched = dataframe[dataframe[dataframe.columns[0]].str.replace(' ','').str.lower().str.contains('|'.join(searchfor))].index[0]
     
     columns_names = np.array(*dataframe[dataframe[dataframe.columns[0]].str.replace(' ','').str.lower().str.contains('|'.join(searchfor))][dataframe.columns[0]].str.split('\t'))
-    
+    #columns_names = np.array([col for col in columns_names if ('avg' in col.lower()) | ('time' in col.lower())])
     header_info = pd.DataFrame(dataframe[:index_matched])
     
     dataframe_splitted = dataframe[dataframe.columns[0]].str.split('\t', expand = True)
@@ -41,11 +43,15 @@ def search_timestamp(dataframe):
     final_dataframe.dropna(axis='columns', inplace = True)
     
     final_dataframe.iloc[:, 1:] = final_dataframe.iloc[:, 1:].astype(float)
+
+    final_dataframe = final_dataframe.loc[:, [col for col in final_dataframe.columns if ('avg' in col.lower()) | ('time' in col.lower())]]
+    
+    final_dataframe.columns = [''.join(x.split('_', 2)[:2]) for x in final_dataframe.columns]
     
     return final_dataframe, header_info
 
 
-# In[23]:
+# In[109]:
 
 
 #a, b = search_timestamp(raw_data)
@@ -57,7 +63,7 @@ def search_timestamp(dataframe):
 #b.to_csv(r'pandas.txt', header=None, index=None, sep=' ', mode='a')
 
 
-# In[49]:
+# In[111]:
 
 
 if __name__ == "__main__":
@@ -69,8 +75,8 @@ if __name__ == "__main__":
     df.head()
     df, header_df = search_timestamp(df)
     
-    df.to_csv(f'{file_name}_data_cleaned.csv')
-    header_df.to_csv(f'{file_name}_header_info.txt', header=None, index=None, sep=' ', mode='a')
+    df.to_csv(f'data/{file_name}_data_cleaned.csv')
+    header_df.to_csv(f'headers/{file_name}_header_info.txt', header=None, index=None, sep=' ', mode='a')
     
 
 
